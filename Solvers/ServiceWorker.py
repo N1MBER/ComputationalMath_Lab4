c50bbd255112ed62ae8a0d5b2f4efe8cb33a964d
Maxim <socialfox33@gmail.com>
2020-06-02 17:59:10 +0300

    Final commit

--- Solvers/ServiceWorker.py
+++ Solvers/ServiceWorker.py
@@ -10,29 +10,99 @@ if __name__ == "__main__":
 
 
 class ServiceWorker:
+    mode = 0
     type_equation = 0
     x_values = []
     y_values = []
-    mode = 0
     with_y = 0
     count_of_dots = 0
 
+    ready_data = {1: [[-2, -1, 0, 1],[-2,-1.7,-1.4,-1.1,-0.8,-0.5,-0.2,0.1,0.4,0.7,1],[0,10,20,30,40,50,60,70,80,90]],
+                  2: [[0, np.pi/2,np.pi,3*np.pi/2,2*np.pi],[0,np.pi/6,np.pi/3,np.pi/2,2*np.pi/3,5*np.pi/6,np.pi,7*np.pi/6,4*np.pi/3,3*np.pi/2,5*np.pi/3,11*np.pi/6,2*np.pi],
+                      [0,5*np.pi,10*np.pi,15*np.pi,20*np.pi,25*np.pi,30*np.pi,35*np.pi,40*np.pi,45*np.pi,50*np.pi]],
+                  3: [[0,1,2,3],[0,0.5,1,1.5,2,2.5,3],[0,5,10,15,20,25,30,35,40,45,50]],
+                  4: [[0,1,2,3],[0,0.5,1,1.5,2,2.5,3],[0,5,10,15,20,25,30,35,40,45,50]]
+    }
+    ready_txt = {1: ["[-2, -1, 0, 1]","[-2,-1.7,-1.4,-1.1,-0.8,-0.5,-0.2,0.1,0.4,0.7,1]","[0,10,20,30,40,50,60,70,80,90]"],
+                  2: ["[0, np.pi/2,np.pi,3*np.pi/2,2*np.pi]","[0,np.pi/6,np.pi/3,np.pi/2,2*np.pi/3,5*np.pi/6,np.pi,7*np.pi/6,4*np.pi/3,3*np.pi/2,5*np.pi/3,11*np.pi/6,2*np.pi]",
+                      "[0,5*np.pi,10*np.pi,15*np.pi,20*np.pi,25*np.pi,30*np.pi,35*np.pi,40*np.pi,45*np.pi,50*np.pi]"],
+                  3: ["[0,1,2,3]","[0,0.5,1,1.5,2,2.5,3]","[0,5,10,15,20,25,30,35,40,45,50]"],
+                  4: ["[0,1,2,3]","[0,0.5,1,1.5,2,2.5,3]","[0,5,10,15,20,25,30,35,40,45,50]"]
+    }
+
     def __init__(self, mode):
         self.mode = mode
+        self.x_values = []
+        self.y_values = []
         self.select_equation()
-        self.set_abscissa()
-        self.set_ordinate()
+        self.start()
         interpolator = Interpolation.Interpolator(self.x_values, self.y_values, self.type_equation)
-        interpolator.start()
+        interpolator.finite_differences()
         make_graph(interpolator)
         check_dots(interpolator)
         del interpolator
+
+    def start(self):
+        if self.mode == 2:
+            self.set_abscissa()
+            self.set_ordinate()
+        elif self.mode == 1:
+            while 1:
+                print("Please choose a type\n"
+                      "\t1. small interval and 4-5 dots: " + self.ready_txt[self.type_equation][0] + "\n"
+                      "\t2. small interval and 10-11 dots" + self.ready_txt[self.type_equation][1] + "\n"
+                      "\t3. change value of function for one x\n"
+                      "\t4. big interval and 10-11 dots" + self.ready_txt[self.type_equation][2] + "\n")
+                answer = int(input("Variant: ").strip())
+                if answer == 1:
+                    self.x_values = self.ready_data[self.type_equation][0]
+                    for i in self.x_values:
+                        try:
+                            self.y_values.append(get_function_value(self.type_equation, i))
+                        except ZeroDivisionError:
+                            self.y_values.append(get_function_value(self.type_equation, i + 1e-9))
+                    break
+                elif answer == 2:
+                    self.x_values = self.ready_data[self.type_equation][1]
+                    for i in self.x_values:
+                        try:
+                            self.y_values.append(get_function_value(self.type_equation, i))
+                        except ZeroDivisionError:
+                            self.y_values.append(get_function_value(self.type_equation, i + 1e-9))
+                    break
+                elif answer == 3:
+                    self.x_values = self.ready_data[self.type_equation][1]
+                    for i in self.x_values:
+                        try:
+                            self.y_values.append(get_function_value(self.type_equation, i))
+                        except ZeroDivisionError:
+                            self.y_values.append(get_function_value(self.type_equation, i + 1e-9))
+                    print("Write number of dots which you want to change\n")
+                    number = int(input("Number: ").strip())
+                    if 1 <= number <= len(self.ready_data[self.type_equation][2]):
+                        value = float(input("Value for this dot: ").strip())
+                        self.y_values[number - 1] = value
+                    else:
+                        getReadyAnswer(1)
+                        continue
+                    break
+                elif answer == 4:
+                    self.x_values = self.ready_data[self.type_equation][2]
+                    for i in self.x_values:
+                        try:
+                            self.y_values.append(get_function_value(self.type_equation, i))
+                        except ZeroDivisionError:
+                            self.y_values.append(get_function_value(self.type_equation, i + 1e-9))
+                else:
+                    getReadyAnswer(1)
+                    continue
+
 
     def select_equation(self):
         while 1:
             try:
                 print("Please choose equation:\n"
-                      "\t1. f(x) = x^3 - 2x^2 + 8x\n"
+                      "\t1. f(x) = x^3 - 3x^2 - 8|x|\n"
                       "\t2. f(x) = sin(x) + 2\n"
                       "\t3. f(x) = 2/x + x/2\n"
                       "\t4. f(x) = e^2x - 2\n")
@@ -52,66 +122,28 @@ class ServiceWorker:
 
     def set_abscissa(self):
         while 1:
-            if self.mode == 1:
-                try:
-                    self.x_values = []
-                    print("Please set dot's:")
-                    dots = list(input("Dots: ").strip().split(" "))
-                    if len(dots) < 2:
-                        getReadyAnswer(1)
-                        continue
-                    else:
-                        self.count_of_dots = len(dots)
-                        for i in dots:
-                            self.x_values.append(float(i.strip()))
-                        break
-                except ValueError:
-                    getReadyAnswer(1)
-                    continue
-                except TypeError:
-                    getReadyAnswer(1)
-                    continue
-            elif self.mode == 2:
-                try:
-                    self.x_values = []
-                    count = int(input("How many dot's? : ").strip())
-                    if count < 2:
-                        getReadyAnswer(1)
-                        continue
-                    else:
-                        step = float(input("Which step? : ").strip())
-                        if step <= 0:
-                            getReadyAnswer(1)
-                            continue
-                        else:
-                            start = float(input("Which start point? :").strip())
-                            self.count_of_dots = count
-                            self.x_values.append(start)
-                            i = 1
-                            while i < count:
-                                self.x_values.append(self.x_values[i - 1] + step)
-                                i += 1
-                            break
-                except ValueError:
-                    getReadyAnswer(1)
-                    continue
-                except TypeError:
-                    getReadyAnswer(1)
-                    continue
-            else:
-                return
-
-    def set_ordinate(self):
-        while 1:
             try:
-                print("How do you want to set \'y\':\n"
-                      "\t1. By yourself\n"
-                      "\t2. Default from function\n")
-                answer = int(input("Variant: ").strip())
-                if answer == 1:
-                    self.with_y = 1
-                elif answer == 2:
-                    self.with_y = 0
+                self.x_values = []
+                print("Please set interval [a;b]\n")
+                count = list(input("[a ;b]: ").strip().split())
+                if len(count) == 2:
+                    a = float(count[0].strip())
+                    b = float(count[1].strip())
+                    if b < a:
+                        getReadyAnswer(1)
+                        continue
+                    print("How many dots?\n")
+                    dots = int(input("Dots: ").strip())
+                    if dots < 2:
+                        getReadyAnswer(1)
+                        continue
+                    step = (b - a) / dots
+                    i = a
+                    while i <= b:
+                        self.x_values.append(i)
+                        i += step
+                    self.count_of_dots = dots
+                    break
                 else:
                     getReadyAnswer(1)
                     continue
@@ -121,38 +153,50 @@ class ServiceWorker:
             except TypeError:
                 getReadyAnswer(1)
                 continue
-            self.y_values = []
-            if self.with_y:
-                ordinates = list(input("Ordinates: ").strip().split(" "))
-                if len(ordinates) != self.count_of_dots:
+
+    def set_ordinate(self):
+        while 1:
+            try:
+                print("How do you want to set \'y\':\n"
+                      "\t1. From function\n"
+                      "\t2. All from function and one by yourself\n")
+                answer = int(input("Variant: ").strip())
+                if answer == 1:
+                    for i in self.x_values:
+                        try:
+                            self.y_values.append(get_function_value(self.type_equation, i))
+                        except ZeroDivisionError:
+                            self.y_values.append(get_function_value(self.type_equation, i + 1e-9))
+                    break
+                elif answer == 2:
+                    for i in self.x_values:
+                        try:
+                            self.y_values.append(get_function_value(self.type_equation, i))
+                        except ZeroDivisionError:
+                            self.y_values.append(get_function_value(self.type_equation, i + 1e-9))
+                    print("Write number of dots which you want to change\n")
+                    number = int(input("Number: ").strip())
+                    if 1 <= number <= self.count_of_dots:
+                        value = float(input("Value for this dot: ").strip())
+                        self.y_values[number - 1] = value
+                    else:
+                        getReadyAnswer(1)
+                        continue
+                    break
+                else:
                     getReadyAnswer(1)
                     continue
-                else:
-                    for i in ordinates:
-                        try:
-                            self.y_values.append(get_function_value(self.type_equation, float(i.strip())))
-                        except ZeroDivisionError:
-                            getReadyAnswer(1)
-                            continue
-                        except TypeError:
-                            getReadyAnswer(1)
-                            continue
-                        except ValueError:
-                            getReadyAnswer(1)
-                            continue
-                    break
-            else:
-                for i in self.x_values:
-                    try:
-                        self.y_values.append(get_function_value(self.type_equation, i))
-                    except ZeroDivisionError:
-                        self.y_values.append(get_function_value(self.type_equation, i + 1e-9))
-                break
+            except ValueError:
+                getReadyAnswer(1)
+                continue
+            except TypeError:
+                getReadyAnswer(1)
+                continue
 
 
 def get_function_value(equation, x):
     if equation == 1:
-        return np.power(x, 3) - 2 * np.power(x, 2) + 8 * x
+        return np.power(x, 3) - 3 * np.power(x, 2) - abs(8 * x)
     elif equation == 2:
         return np.sin(x) + 2
     elif equation == 3:
@@ -173,7 +217,8 @@ def check_dots(calculator):
             answer = int(input("Answer: ").strip())
             if answer == 1:
                 dot = float(input("Dot: ").strip())
-                calculator.get_dots_value(dot)
+                result = calculator.get_dots_value(dot)
+                print(result)
                 make_graph(calculator)
             elif answer == 2:
                 break
@@ -211,23 +256,21 @@ def make_graph(calculator):
         plt.grid()
         ax.spines['top'].set_visible(False)
         ax.spines['right'].set_visible(False)
-        equations = {1: "f(x) = x^3 - 2x^2 + 8x",
+        equations = {1: "f(x) = x^3 - 3x^2 - 8|x|",
                      2: "f(x) = sin(x) + 2",
                      3: "f(x) = 2/x + x/2",
                      4: "f(x) = e^2x - 2"}
         plt.title("Graphic of " + equations[calculator.equation_type])
-        if len(calculator.dots_x) == 0:
-            x = np.linspace(min(calculator.x_values), max(calculator.x_values), 100)
-        else:
-            minimum = min(calculator.x_values)
-            maximum = max(calculator.x_values)
+        minimum = min(calculator.x_values)
+        maximum = max(calculator.x_values)
+        if len(calculator.dots_x) != 0:
             for i in calculator.dots_x:
                 if i < minimum:
                     minimum = i
                 elif i > maximum:
                     maximum = i
-            x = np.linspace(minimum, maximum, 100)
-        first_equation = [calculator.calculate(i) for i in x]
+        x = np.linspace(minimum, maximum, 100)
+        first_equation = [calculator.interpolation(i) for i in x]
         second_equation = [get_function_value(calculator.equation_type, k) for k in x]
         plt.plot(x, first_equation, color='b', linewidth=2)
         plt.plot(x, second_equation, color='r', linewidth=2)
--- main.py
+++ main.py
@@ -12,21 +12,21 @@ while 1:
         answer = int(input("Please choose a variant: ").strip())
         if answer == 1:
             while 1:
-                print("Please choose a variant of interpolation:\n"
-                      "\t1. With a given non-constant step.\n"
-                      "\t2. With a constant step.\n"
-                      "\t3. Back\n")
-                var = int(input("Please choose a variant: ").strip())
-                if var == 1:
+                print("Please choose a mode:\n"
+                      "\t1. With ready data\n"
+                      "\t2. With your data\n")
+                num = int(input("Variant: ").strip())
+                if num == 1:
                     worker = ServiceWorker.ServiceWorker(1)
                     del worker
                     continue
-                elif var == 2:
+                elif num == 2:
                     worker = ServiceWorker.ServiceWorker(2)
                     del worker
                     continue
-                elif var == 3:
-                    break
+                else:
+                    ServiceWorker.getReadyAnswer(1)
+                    continue
         elif answer == 2:
             print("Exit...")
             break
